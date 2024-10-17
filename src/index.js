1bcad9854f4feec64c7b585b796f568d23768bd8
Giulliano Antonini <giullianoantonini@gmail.com>
2024-10-17 17:46:23 -0300

    Finalizado

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function soma() {
+    const nome = "Giulliano";
+    const idade = 31;
+    console.log(idade);
+    console.log(nome);
+    return `Meu nome é ${nome}, e tenho ${idade} anos.`;
+}
+console.log(soma());
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function soma(): string {
+  const nome: string = "Giulliano";
+  const idade: number = 31;
+
+  console.log(idade);
+  console.log(nome);
+  return `Meu nome é ${nome}, e tenho ${idade} anos.`;
+}
+
+console.log(soma());
